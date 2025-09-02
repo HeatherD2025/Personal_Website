13e9e0fa7358df60e91114b1f200669fe82630fa
heatherd2025 <heatherdeliso@gmail.com>
2025-09-02 14:19:09 -0400

    more styling, project section started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,7 +69,7 @@ export default function Home() {
               </div>
 
             {/* Section separater */}
-            <div className="sectionSpace"></div>
+            {/* <div className="sectionSpace"></div> */}
 
               {/* Projects section */}
               <div className="full-width-grid">
@@ -77,7 +77,7 @@ export default function Home() {
               </div>
 
             {/* Section separater */}
-            <div className="sectionSpace"></div>
+            {/* <div className="sectionSpace"></div> */}
 
               {/* Contact Form section */}
               <div className="full-width-grid">
