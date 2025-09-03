0721fa962467f9cc30f56c7f7353bf137347ded5
heatherd2025 <heatherdeliso@gmail.com>
2025-09-03 11:12:41 -0400

    first project tile styled

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,11 +1,18 @@
-import NavBar from "./Navbar"
 
-export default function Cv() {
+
+export default function CV() {
 
 return (
    <>
      <section id="cv" className="section">
       <div className="sectionSpacer"></div>
+
+        {/* Between section accent elements*/}
+          {/* <div className="betweenSectionContainer">
+            <div className="betweenSectionAccentLine"></div>
+            <div className="betweenSectionAccentBox"></div>
+            <div className="betweenSectionFloatingAccentBox"></div>
+          </div> */}
         TESTING
      </section>
    </>
