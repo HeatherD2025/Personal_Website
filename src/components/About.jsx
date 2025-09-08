62b273ee32588d512f30596019abfa5b00e2e4be
heatherd2025 <heatherdeliso@gmail.com>
2025-09-08 16:42:39 -0400

    resizing improvements

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,13 @@
 import NavBar from "../components/Navbar";
 import "../styles/main.css";
+import html from "../assets/images/icons/html.png";
+import css from "../assets/images/icons/css.png";
+import javascript from "../assets/images/icons/javascript.png";
+import json from "../assets/images/icons/json.png";
+import nodeJs from "../assets/images/icons/nodeJs.png";
+import postgresql from "../assets/images/icons/postgresqlLogo.png";
+import prisma from "../assets/images/icons/prismalogo.png";
+import react from "../assets/images/icons/reactlogo.png";
 
 export default function About() {
 
@@ -23,27 +31,39 @@ export default function About() {
                   <div className="floatingAccentBox4"></div>
                 </div>
 
-                  <div className="aboutParagraph">
-                      <div>
-                        I’m a fullstack developer with a background in design, operations, and hospitality—and a lifelong curiosity about science, technology, and how things work.
-                        After 12+ years leading teams and coordinating events, I made the leap into web development through an immersive Fullstack Academy bootcamp. 
-                        I’m now focused on building creative, user-first applications with responsive design across all known dimensions.
+                  <div className="aboutParagraphWrapper">
+                      <div className="aboutParagraph">
+                        I was a curious kid - passionate about technology and science with a knack for visualizing functionality and design. 
+                        Naturally inquisitive and excited to understand cause-and-effect processes, I've always had a persistant drive to learn something new. 
+                        After 12+ years leading teams and coordinating events, I was craving a new challenge, so I made the leap into web development 
+                        through an immersive Fullstack Academy bootcamp. Now, as a fullstack developer with a background in design, operations, and hospitality,
+                        I'm focused on building user-first applications with responsive design. Ready to join a new team helping create the extraordinary!
                       </div>
-                       <ul>
-                        <li>Graduate of Fullstack Academy's immersive web development program (JavaScript, React, Node.js, Express, SQL)</li>
-                        <li>Background in event operations, team leadership, and client-focused communication</li>
-                        <li>Passionate about science, technology, and turning complex ideas into intuitive interfaces</li>
-                        <li>Driven to design responsive, accessible web experiences that work everywhere</li>
-                        <li>Collaborative mindset with a creative eye and strategic approach</li>
-                       </ul>
-                      Whether you’re building tools for Earth or beyond, I’d love to be part of your next mission. Let’s create something extraordinary—together.
+                       <div className="bulletPointWrapper">
+                          <div className="bulletPoint">Graduate of Fullstack Academy's immersive web development program (JavaScript, React, Node.js, Express, SQL)</div>
+                            <div className="toolsContainer">
+                              <img className="toolIcon" src={html} alt="html logo"></img>
+                              <img className="toolIcon" src={css} alt="css logo"></img>
+                              <img className="toolIcon" src={javascript} alt="js logo"></img>
+                              <img className="toolIcon" src={json} alt="json logo"></img>
+                              <img className="toolIcon" src={react} alt="react logo"></img>
+                              <img className="toolIcon" src={nodeJs} alt="nodeJs logo"></img>
+                              <img className="toolIcon" src={postgresql} alt="postregsql logo"></img>
+                              <img className="toolIcon" src={prisma} alt="prisma logo"></img>
+                            </div>
+                          {/* <div className="bulletPoint">Background in event operations, team leadership, and client-focused communication</div>
+                          <div className="bulletPoint">Passionate about turning complex ideas into intuitive interfaces</div>
+                          <div className="bulletPoint">Driven to design responsive, accessible web experiences that work everywhere</div>
+                          <div className="bulletPoint">Collaborative mindset with a creative eye and strategic approach</div> */}
+                       </div>
                   </div>
-              </div>
+              
                   <div className="betweenSectionContainer">
                     <div className="betweenSectionAccentLine"></div>
                     <div className="betweenSectionAccentBox"></div>
                     <div className="betweenSectionFloatingAccentBox"></div>
                   </div>
+                </div>
             </div>
           </section>
         </>
--- src/pages/OPSGProjectDetail.jsx
+++ src/pages/OPSGProjectDetail.jsx
@@ -1,4 +1,7 @@
-
+import "../styles/projectPages.css";
+import postgresql from "../assets/images/icons/postgresqlLogo.png";
+import prisma from "../assets/images/icons/prismalogo.png";
+import react from "../assets/images/icons/reactlogo.png";
 
 export default function OPSGProjectDetail() {
 
@@ -6,11 +9,40 @@ return (
    <>
     <div className="container">
       <div className="background">
-          <div className="pageGrid">
+        <div className="projectGrid">
 
+            <div className="projectDetailHeaderWrapper">
+              <div className="projectDetailHeaderLeft">projects</div>
+              <div className="verticalDividerLine"></div>
+              <div className="projectDetailHeaderRight">On Point Solutions Group</div>
+            </div>
 
+            <div className="projectTitle">On Point Solutions Group</div>
+              <div className="projectDescription">
+                On Points Solutions Group is a medical staff credientialing company.  
+                This site was desiged to entice new clients and includes an option for existing accounts to log in.
+                From the user dashboard, users can see any outstanding invoices and connect to the Intuit payment system.
+              </div>
+              <a className="projectLink" href="https://onpointsolutions.netlify.app/#/" target=" ">view site</a>
+            <div className="accentLine6"></div>
+            <div className="accentBox5"></div>
 
-          </div>
+            <div className="toolIconsb">
+               <img className="toolIcon1b" src={react} alt="react logo"></img>
+               <img className="toolIcon2b" src={prisma} alt="prisma logo"></img>
+               <img className="toolIcon3b" src={postgresql} alt="postgresql logo"></img>
+            </div>  
+
+            <div className="accentBox6"></div>
+              <div className="projectDescription2">
+                testing asf canfckjnfkw asdjoelfewlnvelwnclkeandflewncleskcmneldkcmlcmac
+              </div>
+
+            <div className="accentBox7"></div>
+              <div className="projectDescription3">
+                arbgiprhegfinwa;fnewklcrmgwexs rlkefjs krcjtd bejnvks.krafx;kls.n glk. cdnxd
+              </div>
+        </div>
       </div>
     </div>
    </>
